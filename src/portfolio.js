1762a84c5d10863c2a758a795bb5d16630d66056
duclong1311 <duclong1311.work@gmail.com>
2024-12-25 22:49:49 +0700

    Edit resume information

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Jayce Tran",
+  title: "Hi all, I'm Long",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Front End Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
@@ -33,13 +33,11 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/duclong1311",
+  linkedin: "https://www.linkedin.com/in/long-tran-25549a343/",
+  gmail: "duclong1311.work.com",
+  facebook: "https://www.facebook.com/trduclong1311/",
+  stackoverflow: "https://stackoverflow.com/users/27407489/duc-long",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -49,10 +47,10 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "FRONT END DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ Develop highly interactive Front end / User Interfaces for your web applications"
     ),
     emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
     emoji(
@@ -89,8 +87,8 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-node"
     },
     {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
+      skillName: "php",
+      fontAwesomeClassname: "fab fa-php"
     },
     {
       skillName: "npm",
@@ -101,8 +99,8 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fas fa-database"
     },
     {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
+      skillName: "bootstrap",
+      fontAwesomeClassname: "fab fa-bootstrap"
     },
     {
       skillName: "firebase",
@@ -126,23 +124,17 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
+      schoolName: "Hanoi University of Industry",
+      logo: require("./assets/images/unnamed.png"),
+      subHeader: "Bachelor of Information Systems",
+      duration: "09/2020 - 06/2024",
+
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "CodeGym Programmer Training Center",
+      logo: require("./assets/images/1631407138886.jpg"),
+      subHeader: "Frontend Web Developer Training course Certificate",
+      duration: "06/2024 - 12/2024",
     }
   ]
 };
@@ -158,11 +150,11 @@ const techStack = {
     },
     {
       Stack: "Backend",
-      progressPercentage: "70%"
+      progressPercentage: "30%"
     },
     {
       Stack: "Programming",
-      progressPercentage: "60%"
+      progressPercentage: "70%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -174,29 +166,11 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
-    },
-    {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
       role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      company: "Top Engineering",
+      companylogo: require("./assets/images/top-logo1.png"),
+      date: "May 2023 – July 2023",
+      desc: "Joined the Development team and, within three months, contributed to building the TERP application. This application streamlines storage management for the company, including functionalities for import/export operations, provider and customer management, stock tracking, and multi-currency handling across various providers and customers.",
     }
   ]
 };
@@ -206,35 +180,42 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  title: "Projects",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/coffee-shop.jpg"),
+      projectName: "CodeGym Coffee",
+      projectDesc: "A website designed for introducing & managing a coffee shop.",
       footerLink: [
         {
           name: "Visit Website",
           url: "http://saayahealth.com/"
+        },
+        {
+          name: "Source Code",
+          url: "https://github.com/team1-c05wfe/coffee-smart-system"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/bookee.png"),
+      projectName: "Bookee",
+      projectDesc: "A website for selling books, inspired by the Shopee e-commerce platform.",
       footerLink: [
         {
           name: "Visit Website",
           url: "http://nextu.se/"
+        },
+        {
+          name: "Source Code",
+          url: "https://github.com/duclong1311/tiki-bookstore-clone"
         }
       ]
     }
@@ -252,50 +233,73 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "Responsive Web Design",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
+        "Completed a comprehensive course on building responsive websites using HTML5, CSS3, Flexbox, and CSS Grid. Gained expertise in designing layouts that adapt seamlessly to various screen sizes and devices while ensuring inclusivity through applied accessibility techniques.",
+      image: require("./assets/images/freecodecamp-icon-2048x1381-57apvagh.png"),
       imageAlt: "Google Code-In Logo",
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+          url: "https://www.freecodecamp.org/certification/duclong1311/responsive-web-design"
         },
         {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+          name: "CSS3",
+          url: "https://www.freecodecamp.org/certification/duclong1311/responsive-web-design"
         },
         {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          name: "Responsive",
+          url: "https://www.freecodecamp.org/certification/duclong1311/responsive-web-design"
+        },
+        {
+          name: "Animation",
+          url: "https://www.freecodecamp.org/certification/duclong1311/responsive-web-design"
         }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "JavaScript Algorithms and Data Structures",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
+        "Completed an in-depth course covering core JavaScript concepts, including ES6 syntax, object-oriented programming, and functional programming. Gained hands-on experience with algorithms and data structures such as arrays, stacks, queues, linked lists, and binary trees.",
+      image: require("./assets/images/freecodecamp-icon-2048x1381-57apvagh.png"),
       imageAlt: "Google Assistant Action Logo",
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Certification",
+          url: "https://www.freecodecamp.org/certification/duclong1311/javascript-algorithms-and-data-structures-v8"
+        },
+        {
+          name: "JavaScript",
+          url: "https://www.freecodecamp.org/certification/duclong1311/javascript-algorithms-and-data-structures-v8"
+        },
+        {
+          name: "OOP",
+          url: "https://www.freecodecamp.org/certification/duclong1311/javascript-algorithms-and-data-structures-v8"
+        },
+        {
+          name: "DSA",
+          url: "https://www.freecodecamp.org/certification/duclong1311/javascript-algorithms-and-data-structures-v8"
         }
       ]
     },
 
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
+      title: "Scrum Essence",
+      subtitle: "Acquired foundational knowledge of the Scrum framework, including its roles, artifacts, and ceremonies. Learned how to apply Agile principles to foster collaboration, enhance team productivity, and deliver value incrementally.",
+      image: require("./assets/images/scrum-agile.png"),
       imageAlt: "PWA Logo",
       footerLink: [
-        {name: "Certification", url: ""},
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Certification",
+          url: "https://drive.google.com/file/d/1Jqms0UFpn7-B9NeIEr6TjK8_R3Han-Zi/view?usp=sharing"
+        },
+        {
+          name: "Scrum",
+          url: "https://drive.google.com/file/d/1Jqms0UFpn7-B9NeIEr6TjK8_R3Han-Zi/view?usp=sharing"
+        },
+        {
+          name: "Agile",
+          url: "https://drive.google.com/file/d/1Jqms0UFpn7-B9NeIEr6TjK8_R3Han-Zi/view?usp=sharing"
         }
       ]
     }
@@ -324,7 +328,7 @@ const blogSection = {
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -343,7 +347,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -356,7 +360,7 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Resume Section
@@ -365,22 +369,22 @@ const resumeSection = {
   subtitle: "Feel free to download my resume",
 
   // Please Provide with Your Podcast embeded Link
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+84-969157294",
+  email_address: "duclong1311.work@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
